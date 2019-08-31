31daf98c7bdf6f9e2208aebe46df8502de2521a0
tonyfromundefined <jeehyukwon@gmail.com>
2019-08-31 14:00:33 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
+import LogoImage from './Logo.svg'
+
+let frameId: any
+
+function paddingNumber(num: number) {
+  const str = '' + num
+  const pad = '00'
+  const ans = pad.substring(0, pad.length - str.length) + str
+
+  return ans
+}
 
 const App: React.FC = () => {
+  const [hour, setHour] = useState('')
+  const [minute, setMinute] = useState('')
+  const [second, setSecond] = useState('')
+
+  const awaitRequestFrameAndUpdateCount = useCallback(() => {
+    frameId = requestAnimationFrame(() => {
+      const date = new Date()
+
+      setHour(paddingNumber(date.getHours()))
+      setMinute(paddingNumber(date.getMinutes()))
+      setSecond(paddingNumber(date.getSeconds()))
+
+      return awaitRequestFrameAndUpdateCount()
+    })
+  }, [])
+
+  useEffect(() => {
+    awaitRequestFrameAndUpdateCount()
+
+    return () => window.cancelAnimationFrame(frameId)
+  }, [awaitRequestFrameAndUpdateCount])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-Logo'>
+        <img src={LogoImage} alt='logo' />
+      </div>
+      <div className='App-Clock'>
+        {hour}:{minute}:{second}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
